48c38add2217c8b596e6b51e25caa9c2cada77ad
Denis Khabibullin <denxabi@gmail.com>
2019-11-27 10:55:35 +0500

    Данные сервера и базы

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace PostgresAppConsole
     {
         static void Main(string[] args)
         {
+            string server = "localhost";
+            string port = "5434";
+            string serverUserId = "postgres";
+            string serverPassword = "O4fU32kCs3";
+            string databaseName = "postgres";
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostgresAppConsole
+{
+    class UserModel
+    {
+    }
+}
