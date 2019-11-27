db40b9e0295612ceda11a1ed2b256532069167b2
Denis Khabibullin <denxabi@gmail.com>
2019-11-27 10:31:20 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PostgresAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
